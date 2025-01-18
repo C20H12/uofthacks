b3e52722e404140b40c537365aba2ec94499b908
SenseiPrimexus <kevin20383sabis@gmail.com>
2025-01-18 11:56:01 -0500

    Init Auth0

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+<Auth0Provider
+    domain="dev-mb0kvpqdd2pi0ptj.us.auth0.com"
+    clientId="UirI5b3iRNpot6Rc3fTfL4lFCteRFX5J"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </StrictMode>,
-)
+  </Auth0Provider>,
+);
+
